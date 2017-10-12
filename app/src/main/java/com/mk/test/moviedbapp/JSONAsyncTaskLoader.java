90b31086238c73594a0d4ae5b1db9cbf7ae0e3b5
Michael Khuong <michael.khuong@bartrendr.com>
2017-10-12 16:37:30 -0700

    minor fix

--- app/src/main/java/com/mk/test/moviedbapp/JSONAsyncTaskLoader.java
+++ app/src/main/java/com/mk/test/moviedbapp/JSONAsyncTaskLoader.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -20,7 +18,7 @@ import java.net.URLConnection;
 
 public class JSONAsyncTaskLoader extends AsyncTaskLoader<JSONObject> {
 
-    private static String URLHit = "https://api.themoviedb.org/3/movie/upcoming?api_key=3e58c17f67f7e33ee9d262afaaf16fa5&language=en-US";
+    private static String URLHit = "https://api.themoviedb.org/3/movie/upcoming?api_key=3e58c17f67f7e33ee9d262afaaf16fa5&language=en-US&page=1";
     private static String TAG = "JSON AsyncTask Loader";
 
     private JSONObject mJSONObject;
@@ -43,11 +41,8 @@ public class JSONAsyncTaskLoader extends AsyncTaskLoader<JSONObject> {
 
         URLConnection urlConnection = null;
         BufferedReader bufferedReader = null;
-        int totalPages;
 
         try {
-            JSONArray jsonObjectList = new JSONArray();
-
             URL url = new URL(URLHit);
             urlConnection = url.openConnection();
             bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -59,35 +54,7 @@ public class JSONAsyncTaskLoader extends AsyncTaskLoader<JSONObject> {
                 stringBuffer.append(line);
             }
 
-            //return new JSONObject(stringBuffer.toString());
-
-            JSONObject jsonObject =  new JSONObject(stringBuffer.toString());
-            totalPages = jsonObject.getInt("total_pages");
-
-            JSONObject jsonObjectOLD = new JSONObject(stringBuffer.toString());
-
-            for (int i = 0; i <= totalPages; i++) {
-                String urlWithPage = URLHit + "&page=" + (i + 1);
-                urlConnection = url.openConnection();
-                bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-                stringBuffer = new StringBuffer();
-
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuffer.append(line);
-                }
-
-                jsonObject =  new JSONObject(stringBuffer.toString());
-                jsonObjectList = concatJSONArray(jsonObjectList, jsonObject.getJSONArray("results"));
-            }
-
-            JSONObject jo = new JSONObject();
-            jo.put("results",jsonObjectList);
-
-            return jo;
-
-            //return jsonObjectOLD;
-
+            return new JSONObject(stringBuffer.toString());
         } catch(Exception ex) {
             Log.e(TAG, "AsyncTaskData", ex);
             return null;
@@ -107,16 +74,5 @@ public class JSONAsyncTaskLoader extends AsyncTaskLoader<JSONObject> {
     public void deliverResult(JSONObject jSONObject) {
         mJSONObject = jSONObject;
         super.deliverResult(jSONObject);
-    }
-
-    private JSONArray concatJSONArray(JSONArray jsonArray1, JSONArray jsonArray2) throws JSONException {
-        JSONArray resultJSONArray = new JSONArray();
-        for (int i = 0; i < jsonArray1.length(); i++) {
-            resultJSONArray.put(jsonArray1.get(i));
-        }
-        for (int i = 0; i < jsonArray2.length(); i++) {
-            resultJSONArray.put(jsonArray2.get(i));
-        }
-        return resultJSONArray;
     }
 }
